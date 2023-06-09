2f4d33fdd13db4b397d64bf548c54559b9e104c6
Aayush Verma <90939261+04AV@users.noreply.github.com>
2023-06-09 01:48:27 -0700

    Add files via upload

--- double_list.c
+++ double_list.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>  
 
-#define ARR_SIZE 200 // used to set the size of the linked list
-#define RANGE 49    // used to determine the range of numbers that should be in linked list [0-RANGE] inclusive
-#define FORMAT_COLUMNS 8 // used to formate columns in print_list
+#define ARR_SIZE 200 // used to set the size of the linked node
+#define RANGE 49    // used to determine the range of numbers that should be in linked node [0-RANGE] inclusive
+#define FORMAT_COLUMNS 8 // used to formate columns in print_node
 #define VERBOSE_PRINT 0 // print VERBOSE or not (if 1, then when printing, the following thing will be printed for each node:
                         // - value
                         // - prev node's address
@@ -19,11 +19,17 @@
 int malloc_num = 0;
 
 
-// linked list struct (typedef to just list)
-typedef struct list {
+// linked node struct (typedef to just node)
+typedef struct node {
     int value;
-    struct list *next;
-    struct list *prev;
+    struct node *next;
+    struct node *prev;
+} node;
+
+// doubly linked list struct (typedef to just double_list)
+typedef struct double_list {
+    node *first;
+    node *last;
 } double_list;
 
 
@@ -35,127 +41,133 @@ void my_free(void *p) {
 }
 
 
-// free_list function recursively frees each node of the double list
+// free_node function recursively frees each node of the double node
 // since the address itself is cleared, we don't have to worry about whether connections will mess anything up
 // (in other words if a node is deleted, all connections to it will then point to NULL due to this)
-// uses my_free() function (see line 23)
-void free_list(double_list *list) {
-    while (list != NULL) {
-        double_list *node = list;
-        list = list->next;
-        my_free(node);
+// uses my_free() function (see line 38)
+void free_list(double_list *root) {
+    node *n = root->first;
+    while (n != NULL) {
+        node *temp = n;
+        n = n->next;
+        my_free(temp);
     }   
+    my_free(root);
 }
 
 
-// my_malloc function that calls malloc() and also increments malloc_num (see line 10)
+// my_malloc function that calls malloc() and also increments malloc_num (see line 19)
 void* my_malloc(int size) {
     malloc_num++;
     return malloc(size);
 }
 
 
-// iterates over the list and prints:
+// iterates over the node and prints:
 // -  current node's value
 // -  previous node's address
 // -  current node's address
 // -  next node's address
-// if you want to make sure that the connections are correct you can use this function rather than print_list
+// if you want to make sure that the connections are correct you can use this function rather than print_node
 // in main
-void print_list_full(double_list *list) {
-    while (list != NULL) {
-        printf("value: %11d\tprev: %15p\taddress: %15p\tnext: %15p\n",list->value, (void *) list->prev, (void *) list, (void *) list->next);
-        list = list->next;
+void print_node_full(node *node) {
+    while (node != NULL) {
+        printf("value: %11d\tprev: %15p\taddress: %15p\tnext: %15p\n",node->value, (void *) node->prev, (void *) node, (void *) node->next);
+        node = node->next;
     }
 }
 
 
-// prints the values in the list
-void print_list(double_list *list) {
+// prints the values in the node
+void print_node(node *node) {
     int count = 0;
-    while (list != NULL) {
+    while (node != NULL) {
         count = (count + 1) % FORMAT_COLUMNS;
-        printf("%11d, ", list->value);
+        printf("%11d, ", node->value);
         if (count == 0) printf("\n");
-        list = list->next;
+        node = node->next;
     }
     printf("\n");
 }
 
 
-// append() appends an integer to the front of the list (like pushing to a stack)
+// append() appends an integer to the front of the node (like pushing to a stack)
 // - 
 // args:
-// double_list *list: current root node of list
+// node *n: current root node of node
 // int data: data to be appended
 // -
 // returns:
 // pointer to new root node with data as its value
-double_list* append(double_list *list, int data) {
-    double_list *d = my_malloc(sizeof(double_list));
+node* append(node *n, int data) {
+    node *d = my_malloc(sizeof(node));
     d->value = data;
     d->prev = NULL;
-    d->next = list;
-    if (list != NULL) list->prev = d;
+    d->next = n;
+    if (n != NULL) n->prev = d;
     return d;
 }
 
 
-// initialize() makes a doubly linked list from an array
+// initialize() makes a doubly linked node from an array
 double_list* initialize(int (*arr)[], int size) {
-    double_list *ret = NULL;
+    double_list *ret = my_malloc(sizeof(double_list));
+    node *root = NULL;
     for (int i = size - 1; i > -1; i--) {
-        ret = append(ret, *(*arr + i));
+        root = append(root, *(*arr + i));
+        if (i == size - 1) ret->last = root;
     }
+    ret->first = root;
     return ret;
 }
 
 
-// delete() deletes a node from the list and returns the previous node with the updated connections to next and prev
+// delete() deletes a node from the node and returns the previous node with the updated connections to next and prev
 // -
 // args:
-// double_list *list: a pointer corresponding to the address of the node to be deleted
+// node *n: a pointer corresponding to the address of the node to be deleted
 // -
 // returns:
-// a double_list* pointer corresponding to the address of the previous node. this node's internal variables for 
+// a node* pointer corresponding to the address of the previous node. this node's internal variables for 
 // next and prev are updated accordingly and edge cases are accounted for (deleting the first/last node)
-double_list* delete(double_list *list) {
-    // printf("%p\n", list);
-    double_list *previous = list->prev;
-    double_list *old_root = list;
+node* delete(node *n) {
+    // printf("%p\n", node);
+    node *previous = n->prev;
+    node *old_root = n;
     if (previous == NULL) {
-        previous = list->next;
+        previous = n->next;
         previous->prev = NULL; 
     }
-    else previous->next = list->next;
-    if (list->next != NULL) list->next->prev = previous;
+    else previous->next = n->next;
+    if (n->next != NULL) n->next->prev = previous;
     my_free(old_root);
     return previous;
 }
 
 
-// delete_duplicates() deletes the duplicates in the list while maintaining the same order of elements
+// delete_duplicates() deletes the duplicates in the node while maintaining the same order of elements
 // -
 // args:
-// double_list *list: pointer to root node
+// double_list *list: pointer to double_list instance 
 // - 
 // returns:
 // nothing, it's done in place
 // -
-// an int[50] is used to keep track of the number of times each element shows up (the range of numbers in the list
+// an int[50] is used to keep track of the number of times each element shows up (the range of numbers in the node
 // can only be between 0-49). If, when an element "n" is encountered, arr[n] == 0 then arr[n]++ and nothing happens
 // else if an element "n" is encountered and arr[n] > 0 that node is deleted because we've already encountered an element
-// with the same value. This also maintains the relative order to the list's elements and we don't have to sort
+// with the same value. This also maintains the relative order to the node's elements and we don't have to sort
 // or anything. It's done in linear time
 void delete_duplicates(double_list *list) {
     int checker[RANGE + 1];
-    double_list *root = list;
+    node *root = list->first;
     for (int i = 0; i < RANGE + 1; i++) {
         checker[i] = 0;
     }
     while (root != NULL) {
         if (checker[root->value] == 0) checker[root->value]++;
         else root = delete(root);
+        if (root->next == NULL) list->last = root;
         root = root->next;
     }
 }
@@ -163,29 +175,39 @@ void delete_duplicates(double_list *list) {
 int main() {
     // initialize random seed so that random numbers are different every time you run main()
     // and initialize array of randomly generated numbers between 0-ARR_SIZE (inclusive) to be turned into a doubly-linked
-    // list
+    // node
     srand((unsigned)(time(NULL)));
     int arr[ARR_SIZE];
     for (int i = 0; i < ARR_SIZE; i++) {
         arr[i] = rand() % (RANGE + 1);
     }
 
-    // create doubly linked_list from array
+    // create doubly linked_node from array
     int size = sizeof(arr) / sizeof(arr[0]);
-    double_list *list = initialize(&arr, size);
+    double_list *list= initialize(&arr, size);
     
-    // print list before and after deleting duplicates 
-    // (if VERBOSE_PRINT is true then a more in-depth printing of the list will be done, else just the values will
+    // print node before and after deleting duplicates 
+    // (if VERBOSE_PRINT is true then a more in-depth printing of the node will be done, else just the values will
     // be printed) see line 8
     printf("\nlist before deleting duplicates\n");
-    if (VERBOSE_PRINT) print_list_full(list);
-    else print_list(list);
+    if (VERBOSE_PRINT) print_node_full(list->first);
+    else print_node(list->first);
+    // print the head and tail node of the doubly linked list (these should correspond to the first/last numbers shown
+    // the head address should be the exact same even after delete_duplicates
+    // the tail address should change if the tail value changes
+    // you can also change VERBOSE_PRINT to 1 at the top of the program to conclusively make sure head/tail addresses 
+    // are correct if you'd like 
+    printf("-----\ndouble list info:\nhead: %11d - (address = %p)\ntail: %11d - (address = %p)\n", list->first->value, (void *) list->first, list->last->value, (void *) list->last);
 
-    printf("\nlist after deleting duplicates\n");
+    printf("\n\nlist after deleting duplicates\n");
     delete_duplicates(list);
-    if (VERBOSE_PRINT) print_list_full(list);
-    else print_list(list);
+    if (VERBOSE_PRINT) print_node_full(list->first);
+    else print_node(list->first);
+    printf("-----\ndouble list info:\nhead: %11d - (address = %p)\ntail: %11d - (address = %p)\n", list->first->value, (void *) list->first, list->last->value, (void *) list->last);
+    printf("tail's address SHOULD change if it's value changes and it's value should exactly match the last element in the list\n");
+    printf("head's address should never change\n");
     
+    // recursively free all items in doubly linked list
     free_list(list);
     printf("\nmemory leaks: %d\n\n", malloc_num);
     return 0;
